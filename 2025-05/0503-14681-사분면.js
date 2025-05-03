9bf928bab56358f0229e923c1db1667670b4f62a
HJ <a01025105259@gmail.com>
2025-05-04 00:03:38 +0900

    14681

---
+++ 2025-05/0503-14681-사분면.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const [x, y] = fs.readFileSync(0, 'utf-8').trim().split('\n').map(Number);
+
+if (x > 0 && y > 0) console.log(1);
+else if (x < 0 && y > 0) console.log(2);
+else if (x < 0 && y < 0) console.log(3);
+else if (x > 0 && y < 0) console.log(4);
