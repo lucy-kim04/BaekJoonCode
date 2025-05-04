68f2294121a214138b1c245fad4b61ebf9ccbfbf
HJ <a01025105259@gmail.com>
2025-05-04 14:43:03 +0900

    1546 수정

--- 2025-05/05-04/1546-평균.py
+++ 2025-05/05-04/1546-평균.py
@@ -1,5 +1,10 @@
 n = int(input())
 scores = list(map(int, input().split()))
-total = sum(scores)
+topscore = max(scores)
+
+total = 0
+for score in scores:
+    total += (score / topscore) * 100
+
 avg = total / n
 print(avg)
