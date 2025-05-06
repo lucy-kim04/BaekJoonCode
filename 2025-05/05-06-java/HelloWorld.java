c6b359953edcf1ef330cfe7070b4fba3207e925c
HJ <a01025105259@gmail.com>
2025-05-06 11:59:46 +0900

    java 시작

---
+++ 2025-05/05-06-java/HelloWorld.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// System.out.printIn -> console.log, print랑 같은 애
+// public -> 다른데서도 쓸 수 있게 공개로
+// static -> 클래스명으로 접근 가능
+// void -> 리턴값이 없음(실행만 하고 끝)
+// main -> 메인 함수(진입점)
+// String[] args -> 매개변수
+
+// Main: 이건 클래스 이름 (자바는 클래스 중심 언어야)
+
+// public static void main(...): 자바 프로그램 시작점
+
+// System.out.println(...): 출력 함수 (자바의 console.log() / print() 같은 거)
+
