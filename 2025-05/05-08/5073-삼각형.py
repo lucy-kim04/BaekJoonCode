2468bef87479a241c4a1e5abe3080287d4b3dce5
lucy-kim04 <a01025105259@gmail.com>
2025-05-08 20:04:30 +0900

    5073

---
+++ 2025-05/05-08/5073-삼각형.py
@@ -0,0 +1,12 @@
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == 0:
+        break
+    if a + b <= c or b + c <= a or a + c <= b:
+        print("Invalid")
+    elif a == b == c:
+        print("Equilateral")
+    elif a == b or b == c or a == c:
+        print("Isosceles")
+    else:
+        print("Scalene")
